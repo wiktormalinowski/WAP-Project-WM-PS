d2a5715bdcd269e60a28ee547e27908486efd2f8
falamilol <suterski17@gmail.com>
2023-05-10 18:17:12 +0200

    DeliveryOption DTO, Repository and Service added.

---
+++ src/main/java/com/uep/wap/dto/DeliveryOptionDTO.java
@@ -0,0 +1,17 @@
+package com.uep.wap.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeliveryOptionDTO {
+
+    private String delivery_company;
+
+    private float delivery_price;
+
+    private String description;
+
+
+}
