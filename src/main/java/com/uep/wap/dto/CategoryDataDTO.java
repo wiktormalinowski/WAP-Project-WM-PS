190bd197b917d155d99fe4592dad2140fbbbc4bf
Patryk Suterski <suterski17@gmail.com>
2023-05-13 21:06:11 +0200

    Added category classes.

---
+++ src/main/java/com/uep/wap/dto/CategoryDataDTO.java
@@ -0,0 +1,16 @@
+package com.uep.wap.dto;
+
+import java.util.List;
+
+public class CategoryDataDTO {
+
+    List<CategoryDTO> categoryDTOList;
+
+    public List<CategoryDTO> getCategoryDTOList() {
+        return categoryDTOList;
+    }
+
+    public void setCategoryDTOList(List<CategoryDTO> categoryDTOList) {
+        this.categoryDTOList = categoryDTOList;
+    }
+}
---
+++ src/main/java/com/uep/wap/service/CategoryService.java
@@ -0,0 +1,21 @@
+package com.uep.wap.service;
+
+import com.uep.wap.dto.CategoryDTO;
+import com.uep.wap.model.Category;
+import com.uep.wap.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public void addCategory(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        category.setCategory_name(categoryDTO.getCategory_name());
+        categoryRepository.save(category);
+        System.out.println("Category added!");
+    }
+}
