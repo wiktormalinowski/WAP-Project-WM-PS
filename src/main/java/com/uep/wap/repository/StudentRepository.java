6d985b55ff55da5cf0c9115f956575f0cf4a3e15
wiktormalinowski <wiktor.malina01@gmail.com>
2023-06-10 13:17:42 +0200

    .gitignore fixes + removed Student classes

--- src/main/java/com/uep/wap/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uep.wap.repository;
-
-import com.uep.wap.model.Student;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, Integer> {
-}
