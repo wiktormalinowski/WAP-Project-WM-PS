d53a4bd76c3d4f50193cbda65fd64c16da2f0b59
Patryk Suterski <75785487+falamilol@users.noreply.github.com>
2023-06-14 09:18:59 +0200

    bid class

---
+++ src/main/java/com/uep/wap/model/Bid.java
@@ -0,0 +1,34 @@
+package com.uep.wap.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "bid")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bid {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bid_id")
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "bid_amount")
+    private float bid_amount;
+
+     @ManyToOne
+    @JoinColumn(name = "auction_id")
+    private Auction auction;
+}
